690fc6d0d20e177c6eaf648a968d18b97ca8f685
Mitchell <mhogle@bizinta.com>
2024-04-13 22:46:18 -0700

    removed half-baked methods

--- scripts/summons/Summon.cs
+++ scripts/summons/Summon.cs
@@ -6,10 +6,4 @@ namespace BFO.G.GoneFishin;
 public abstract class Summon 
 {
 	[JsonProperty] protected string spriteId = string.Empty;
-	
-	public AnimatedSprite2D InstantiateSprite(GameManager game) => 
-		game.(this.spriteId);
-	
-	public AnimatedSprite2D InstantiateThumbnail(GameManager game) =>
-		game.InstantiateSummonSprite(this.spriteId);
 }
